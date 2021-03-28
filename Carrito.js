f4c6384cac19c45e167fe720bcfcfcb7bc4398c9
Yisusmomoa <bjls29@hotmail.com>
2021-03-28 04:08:21 -0600

    se acabarón las pantallas necesarias, faltan corregir colores, agregar más animaciones y dashboards

---
+++ Carrito.js
@@ -0,0 +1,17 @@
+window.onscroll = function(){
+    AddOrRemove()
+};
+
+var Nav = document.getElementById("EjNav");
+var NavPosition = Nav.offsetTop;
+
+function AddOrRemove(){
+    if(window.pageYOffset >= NavPosition){
+        Nav.classList.add("Paste");
+    }
+    else{
+        Nav.classList.remove("Paste");
+    }
+}
+
+
